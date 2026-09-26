333f0e409306a69e1189e99225716f3aa565a98c
Nguyễn Anh Đức <canhphong1103@gmail.com>
2026-09-26 12:07:27 +0700

    xử lý giao dịch Transaction

--- src/main/java/com/duc/ecommerce/service/InvoiceService.java
+++ src/main/java/com/duc/ecommerce/service/InvoiceService.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -36,7 +37,9 @@ public class InvoiceService {
     ProductRepository productRepository;
     ProductMapper productMapper;
 
+    @Transactional
     public InvoiceResponse create(InvoiceCreateRequest request){
+        //int x = 1/0;
         Invoice invoice = new Invoice();
 
         Authentication authentication =
